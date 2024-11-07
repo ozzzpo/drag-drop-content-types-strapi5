3c2b321015e40b150bedd7067706476de76821e4
cslegany <cslegany@synerinsoft.com>
2024-11-07 17:32:06 +0100

    improved settings UI

--- admin/src/pages/Settings.tsx
+++ admin/src/pages/Settings.tsx
@@ -5,234 +5,173 @@ import { Page, Layouts } from '@strapi/strapi/admin';
 //migration guide
 // https://design-system-git-main-strapijs.vercel.app/?path=/docs/getting-started-migration-guides-v1-to-v2--docs
 import {
-    Flex,
-    Box,
-    Button,
-    Grid,
-    Typography,
-    Field,
-    TextInput,
-    Toggle,
+	Flex,
+	Box,
+	Button,
+	Grid,
+	Typography,
 } from '@strapi/design-system';
 
 import { Information, Check } from '@strapi/icons';
 import { PluginSettingsResponse, PluginSettingsBody } from '../../../typings';
 import { useIntl } from 'react-intl';
 import { getTranslation as getTrad } from '../utils/getTranslation';
-import SettingsTooltip from '../components/SettingsTooltip';
-
-interface SettingsResponse {
-    body: string;
-}
+import SettingsTextField from '../components/SettingsTextField';
+import SettingsToggleField from '../components/SettingsToggleField';
 
 const Settings = () => {
-    const { formatMessage } = useIntl();
+	const { formatMessage } = useIntl();
 
-    const isMounted = useRef(true);
-    const { get, post } = useFetchClient();
+	const isMounted = useRef(true);
+	const { get, post } = useFetchClient();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const { data } = await get<PluginSettingsResponse>(`/drag-drop-content-types/settings`);
-            setSettings(data.body);
-            setIsLoading(false);
-        }
-        fetchData();
+	useEffect(() => {
+		const fetchData = async () => {
+			const { data } = await get<PluginSettingsResponse>(`/drag-drop-content-types/settings`);
+			setSettings(data.body);
+			setIsLoading(false);
+		}
+		fetchData();
 
-        // unmount
-        return () => {
-            isMounted.current = false;
-        };
-    }, [])
+		// unmount
+		return () => {
+			isMounted.current = false;
+		};
+	}, [])
 
-    const defaultSettingsBody: PluginSettingsBody = { title: '', subtitle: '', rank: '', triggerWebhooks: false };
-    const [settings, setSettings] = useState<PluginSettingsBody>(defaultSettingsBody);
-    const [isSaving, setIsSaving] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+	const defaultSettingsBody: PluginSettingsBody = { title: '', subtitle: '', rank: '', triggerWebhooks: false };
+	const [settings, setSettings] = useState<PluginSettingsBody>(defaultSettingsBody);
+	const [isSaving, setIsSaving] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
-    const { toggleNotification } = useNotification();
+	const { toggleNotification } = useNotification();
 
-    const handleSubmit = async () => {
-        if (!settings)
-            return;
+	const handleSubmit = async () => {
+		if (!settings)
+			return;
 
-        setIsSaving(true);
+		setIsSaving(true);
 
-        const res = await post(`/drag-drop-content-types/settings`, {
-            method: 'POST',
-            body: settings
-        });
-        setSettings(res.data.body);
-        setIsSaving(false);
+		const res = await post(`/drag-drop-content-types/settings`, {
+			method: 'POST',
+			body: settings
+		});
+		setSettings(res.data.body);
+		setIsSaving(false);
 
-        toggleNotification({
-            type: 'success',
-            message: formatMessage({
-                id: 'plugin.settings.updated',
-                defaultMessage: 'Settings successfully updated',
-            }),
-        });
-    };
+		toggleNotification({
+			type: 'success',
+			message: formatMessage({
+				id: 'plugin.settings.updated',
+				defaultMessage: 'Settings successfully updated',
+			}),
+		});
+	};
 
-    return (
-        <>
-            <Layouts.Header
-                id="title"
-                title={formatMessage({ id: getTrad("plugin.settings.title") })}
-                subtitle={formatMessage({ id: getTrad("plugin.settings.subtitle") })}
-                primaryAction={
-                    isLoading ? (
-                        <></>
-                    ) : (
-                        <Button
-                            onClick={handleSubmit}
-                            startIcon={<Check />}
-                            size="L"
-                            disabled={isSaving}
-                            loading={isSaving}
-                        >
-                            {formatMessage({ id: getTrad("plugin.settings.buttons.save") })}
-                        </Button>
-                    )
-                }
-            ></Layouts.Header>
-            {isLoading ? (
-                <Page.Loading />
-            ) : (
-                <Layouts.Content>
-                    <Box
-                        background="neutral0"
-                        hasRadius
-                        shadow="filterShadow"
-                        paddingTop={6}
-                        paddingBottom={6}
-                        paddingLeft={7}
-                        paddingRight={7}
-                    >
-                        <Flex size={3} direction="column">
-                            <Box paddingBottom={6}>
-                                <Typography variant="beta" as="h2">
-                                    {formatMessage({ id: getTrad('plugin.settings.field-names') })}
-                                </Typography>
-                            </Box>
-                            <Grid.Root gap={6}>
+	const onUpdateSettings = (fieldName: string, value: string | boolean) => {
+		if (!settings)
+			return;
 
-                                {/* rank */}
-                                <Grid.Item col={6} s={12}>
-                                    <Box padding={0}>
-                                        <Field.Root name="field_rank" hint={formatMessage({ id: getTrad("plugin.settings.rank.hint") })}>
-                                            <Field.Label>
-                                                {formatMessage({ id: getTrad("plugin.settings.rank.label") })}
-                                                <SettingsTooltip tooltip='plugin.settings.rank.tooltip'>
-                                                    <Information />
-                                                </SettingsTooltip>
-                                            </Field.Label>
-                                            <Flex size={12}>
-                                                <TextInput
-                                                    placeholder="Rank"
-                                                    name="content"
-                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                                        setSettings({
-                                                            ...settings,
-                                                            rank: e.target.value,
-                                                        });
-                                                    }}
-                                                    value={settings.rank}
-                                                />
-                                            </Flex>
-                                            <Field.Hint />
-                                        </Field.Root>
-                                    </Box>
-                                </Grid.Item>
+		try {
+			const updatedSettings = { ...settings };
+			(updatedSettings as any)[fieldName] = value;
+			setSettings(updatedSettings);
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-                                {/* title */}
-                                <Grid.Item col={6} s={12}>
-                                    <Box padding={0}>
-                                        <Field.Root name="field_title" hint={formatMessage({ id: getTrad("plugin.settings.title.hint") })}>
-                                            <Field.Label>
-                                                {formatMessage({ id: getTrad("plugin.settings.title.label") })}
-                                                <SettingsTooltip tooltip='plugin.settings.title.tooltip'>
-                                                    <Information />
-                                                </SettingsTooltip>
-                                            </Field.Label>
-                                            <Flex size={12}>
-                                                <TextInput
-                                                    placeholder="Title"
-                                                    name="content"
-                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                                        setSettings({
-                                                            ...settings,
-                                                            title: e.target.value,
-                                                        });
-                                                    }}
-                                                    value={settings.title}
-                                                />
-                                            </Flex>
-                                            <Field.Hint />
-                                        </Field.Root>
-                                    </Box>
-                                </Grid.Item>
+	const checkFormErrors = () => {
+		return !settings.rank;
+	}
 
-                                {/* subtitle */}
-                                <Grid.Item col={6} s={12}>
-                                    <Box padding={0}>
-                                        <Field.Root name="field_subtitle" hint={formatMessage({ id: getTrad("plugin.settings.subtitle.hint") })}>
-                                            <Field.Label>
-                                                {formatMessage({ id: getTrad("plugin.settings.subtitle.label") })}
-                                                <SettingsTooltip tooltip='plugin.settings.subtitle.tooltip'>
-                                                    <Information />
-                                                </SettingsTooltip>
-                                            </Field.Label>
-                                            <Flex size={12}>
-                                                <TextInput
-                                                    placeholder="Subtitle"
-                                                    name="content"
-                                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                                        setSettings({
-                                                            ...settings,
-                                                            subtitle: e.target.value,
-                                                        });
-                                                    }}
-                                                    value={settings.subtitle}
-                                                />
-                                            </Flex>
-                                            <Field.Hint />
-                                        </Field.Root>
-                                    </Box>
-                                </Grid.Item>
+	const hasFormError = checkFormErrors();
 
-                                <Grid.Item col={6} s={12}>
-                                    <Field.Root name="field_triggerWebhooks" hint={formatMessage({ id: getTrad("plugin.settings.triggerWebhooks.hint") })}>
-                                        <Field.Label>
-                                            {formatMessage({ id: getTrad("plugin.settings.triggerWebhooks.label") })}
-                                            <SettingsTooltip tooltip='plugin.settings.triggerWebhooks.tooltip'>
-                                                <Information />
-                                            </SettingsTooltip>
-                                        </Field.Label>
-                                        <Toggle
-                                            checked={settings?.triggerWebhooks ?? false}
-                                            name="triggerWebhooks"
-                                            onLabel={formatMessage({ id: getTrad("plugin.settings.buttons.on") })}
-                                            offLabel={formatMessage({ id: getTrad("plugin.settings.buttons.off") })}
-                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                                setSettings({
-                                                    ...settings,
-                                                    triggerWebhooks: e.target.checked
-                                                });
-                                            }}
-                                        />
-                                        <Field.Hint />
-                                    </Field.Root>
+	return (
+		<>
+			<Layouts.Header
+				id="title"
+				title={formatMessage({ id: getTrad("plugin.settings.title") })}
+				subtitle={formatMessage({ id: getTrad("plugin.settings.subtitle") })}
+				primaryAction={
+					isLoading ? (
+						<></>
+					) : (
+						<Button
+							onClick={handleSubmit}
+							startIcon={<Check />}
+							size="L"
+							disabled={isSaving || hasFormError}
+							loading={isSaving}
+						>
+							{formatMessage({ id: getTrad("plugin.settings.buttons.save") })}
+						</Button>
+					)
+				}
+			></Layouts.Header>
+			{isLoading ? (
+				<Page.Loading />
+			) : (
+				<Layouts.Content>
+					<Box
+						background="neutral0"
+						hasRadius
+						shadow="filterShadow"
+						paddingTop={6}
+						paddingBottom={6}
+						paddingLeft={7}
+						paddingRight={7}
+					>
+						<Flex size={3} direction="column">
+							<Box paddingBottom={6}>
+								<Typography variant="beta" as="h2">
+									{formatMessage({ id: getTrad('plugin.settings.field-names') })}
+								</Typography>
+							</Box>
+							<Grid.Root gap={6}>
 
-                                </Grid.Item>
-                            </Grid.Root>
-                        </Flex>
-                    </Box>
+								{/* rank */}
+								<Grid.Item col={6} s={12}>
+									<Box padding={0} style={{ width: '100%' }}>
+										<SettingsTextField hasTooltip={true} hasHint={true} hasLabel={true} hasPlaceholder={true}
+											fieldName="rank" displayName="rank"
+											required={true} updateItem={onUpdateSettings} value={settings.rank} />
+									</Box>
+								</Grid.Item>
 
-                </Layouts.Content>
-            )}
-        </>
-    );
+								{/* title */}
+								<Grid.Item col={6} s={12}>
+									<Box padding={0} style={{ width: '100%' }}>
+										<SettingsTextField hasTooltip={true} hasHint={true} hasLabel={true} hasPlaceholder={true}
+											fieldName="title" displayName="title"
+											required={false} updateItem={onUpdateSettings} value={settings.title} />
+									</Box>
+								</Grid.Item>
+
+								{/* subtitle */}
+								<Grid.Item col={6} s={12}>
+									<Box padding={0} style={{ width: '100%' }}>
+										<SettingsTextField hasTooltip={true} hasHint={true} hasLabel={true} hasPlaceholder={true}
+											fieldName="subtitle" displayName="subtitle"
+											required={false} updateItem={onUpdateSettings} value={settings.subtitle} />
+									</Box>
+								</Grid.Item>
+
+								<Grid.Item col={6} s={12}>
+									<Box padding={0} style={{ width: '100%' }}>
+										<SettingsToggleField hasTooltip={true} hasHint={true} hasLabel={true}
+											fieldName="triggerWebhooks" displayName="triggerWebhooks"
+											required={false} updateItem={onUpdateSettings} value={settings.triggerWebhooks} />
+									</Box>
+								</Grid.Item>
+							</Grid.Root>
+						</Flex>
+					</Box>
+
+				</Layouts.Content>
+			)}
+		</>
+	);
 };
 
 export default Settings;
