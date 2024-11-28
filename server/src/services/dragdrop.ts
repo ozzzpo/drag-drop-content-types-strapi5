8f8703080c1741d72ddeb216ffd5b740106ac5cb
Andy <andreas@heynanny.com>
2024-11-28 14:38:10 +0100

    feat: update all localizations of element type

--- server/src/services/dragdrop.ts
+++ server/src/services/dragdrop.ts
@@ -3,14 +3,19 @@ import type * as StrapiTypes from '@strapi/types/dist';
 import { PluginSettingsResponse, RankUpdate } from '../../../typings';
 
 const dragdrop = ({ strapi }: { strapi: Core.Strapi }) => ({
-
   getWelcomeMessage() {
     return {
-      body: 'Welcome to Strapi 🚀'
+      body: 'Welcome to Strapi 🚀',
     };
   },
 
-  async sortIndex(contentType: StrapiTypes.UID.CollectionType, start: number, limit: number, locale: string, rankFieldName: string) {
+  async sortIndex(
+    contentType: StrapiTypes.UID.CollectionType,
+    start: number,
+    limit: number,
+    locale: string,
+    rankFieldName: string
+  ) {
     let indexData = {
       sort: {},
       populate: '*',
@@ -31,56 +36,62 @@ const dragdrop = ({ strapi }: { strapi: Core.Strapi }) => ({
    * @param {RankUpdate[]} updates
    * @param {StrapiTypes.UID.CollectionType} contentType
    */
-  async batchUpdate(config: PluginSettingsResponse, updates: RankUpdate[], contentType: StrapiTypes.UID.CollectionType) {
-    const sortFieldName = config.body.rank;
+  async batchUpdate(
+    config: PluginSettingsResponse,
+    updates: RankUpdate[],
+    contentType: StrapiTypes.UID.CollectionType
+  ) {
     const shouldTriggerWebhooks = config.body.triggerWebhooks;
+    const sortFieldName = config.body.rank;
     const results = [];
 
-    strapi["apiUpdate"] = true;
+    strapi['apiUpdate'] = true;
 
     for (const update of updates) {
-      // update entry's rank in db
-      const updatedEntry = await strapi.db.query(contentType).update({
+      const allLocalizations = await strapi.db.query(contentType).findOne({
         where: { id: update.id },
-        data: {
-          [sortFieldName]: update.rank,
-        },
+        populate: ['localizations'],
       });
 
-      if (updatedEntry?.id) {
-        results.push(updatedEntry);
+      const { localizations, ...origin } = allLocalizations;
+      for (const entry of [origin, ...localizations]) {
+        const updatedEntry = await strapi.db.query(contentType).update({
+          where: { id: entry.id },
+          data: {
+            [sortFieldName]: update.rank,
+          },
+        });
 
-        // Trigger webhook listener for updated entry
-        //see: https://forum.strapi.io/t/trigger-webhook-event-from-api/35919/5
-        if (shouldTriggerWebhooks) {
-          const info: Record<string, unknown> = {
-            model: contentType.split('.').at(-1),
-              entry: {
-                id: updatedEntry.id,
-                ...updatedEntry,
-              },
-          }
-                    
-          await strapi.get("webhookRunner").executeListener({
-            event: 'entry.update',
-            info,
-          });
+        if (updatedEntry?.id) {
+          results.push(updatedEntry);
         }
+      }
+
+      // Trigger webhook listener for updated entry
+      //see: https://forum.strapi.io/t/trigger-webhook-event-from-api/35919/5
+      if (shouldTriggerWebhooks) {
+        const info: Record<string, unknown> = {
+          model: contentType.split('.').at(-1),
+          entry: {
+            id: origin.id,
+            ...origin,
+          },
+        };
+
+        await strapi.get('webhookRunner').executeListener({
+          event: 'entry.update',
+          info,
+        });
       }
     }
 
-    strapi["apiUpdate"] = undefined;
+    strapi['apiUpdate'] = undefined;
 
-    if (results?.length !== updates?.length) {
-      throw new Error('Error updating rank entries.');
-    } else {
-      return results.map((entry) => ({
-        id: entry.id,
-        rank: entry[sortFieldName],
-      }));
-    }
+    return updates.map((entry) => ({
+      id: entry.id,
+      rank: entry[sortFieldName],
+    }));
   },
-
 });
 
 export default dragdrop;
